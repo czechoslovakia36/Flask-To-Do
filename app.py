4ab18664d5bb2d1a0ebebb84a814f1fee78dd0ec
Vivek Kumar <clocktower77@protonmail.com>
2024-01-20 16:16:53 +0530

    Initialize sql database

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template, url_for
-
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 
 app= Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+db=SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id= db.Column(db.Integer,primary_key=True)
+    content= db.Column(db.Integer,default=0)
+    date_created= db.Column(db.DateTime,default=datetime.utcnow)
+
 
 '''
 /// --->Relative path 
 ////--->Absolute path
+
+
 '''
 
 
